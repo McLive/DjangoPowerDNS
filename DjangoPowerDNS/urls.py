81873bb148fe32a05f668c9f919957110ab1f9ac
McLive <mail@mclive.eu>
2017-10-11 18:06:38 +0200

    Update license information

--- DjangoPowerDNS/urls.py
+++ DjangoPowerDNS/urls.py
@@ -1,4 +1,22 @@
-"""DjangoPowerDNS URL Configuration
+# --
+# DjangoPowerDNS - A PowerDNS web interface
+# Copyright (C) 2017 McLive
+# --
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU AFFERO General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# --
+
+""""DjangoPowerDNS URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
